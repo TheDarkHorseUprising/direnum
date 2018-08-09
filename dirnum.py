ca2ccb3810e579744abde3abaf3f84d553b247dc
TheDarkHorseUprsising <tdhuprising@gmail.com>
2018-08-09 22:44:40 +0100

    Create dirnum.py

---
+++ dirnum.py
@@ -0,0 +1,24 @@
+#imports
+import requests
+#banner
+print('''________  .__                                               
+\\______ \\ |__|______  ____  __ __  _____      ______ ___.__.
+ |    |  \\|  \\_  __ \\/    \\|  |  \\/     \\     \\____ <   |  |
+ |    `   \\  ||  | \\/   |  \\  |  /  Y Y  \\    |  |_> >___  |
+/_______  /__||__|  |___|  /____/|__|_|  / /\\ |   __// ____|
+        \\/               \\/            \\/  \\/ |__|   \\/''')
+#input
+url=input("url: ")
+wlist=input("wordlist: ")
+
+#get lines
+wlistlines=open(wlist,"r").readlines()
+#loop
+for i in range(0, len(wlistlines)):
+    enum=wlistlines[i].replace("\n","")
+    r=requests.get(url+"/"+enum+"/")
+
+    #if not 404
+    if r.status_code != 404:
+        print(url+"/"+enum+"/ - "+str(r.status_code))
+    
